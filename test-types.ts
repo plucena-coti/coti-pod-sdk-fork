c7a6a0531b4ce0f466a40a28ea808358d88cf3ae
Percival Lucena <plucena@gmail.com>
2026-04-13 10:40:51 -0300

    Remove test-types.ts

--- test-types.ts
+++
@@ -1,6 +0,0 @@
-import { CotiPodCrypto, DataType } from "./src/index.js";
-async function run() {
-  const enc = await CotiPodCrypto.encrypt("Hello from Sepolia!", "testnet", DataType.String);
-  console.log(JSON.stringify(enc, null, 2));
-}
-run();
