88b962d371e7a6cb296260364437b942e8630435
Percival Lucena <plucena@gmail.com>
2026-04-13 15:22:24 -0300

    Clean up leftover test and scratch scripts

--- scripts/fix_sig.ts
+++
@@ -1,5 +0,0 @@
-import { ethers } from "ethers";
-const iface = new ethers.Interface([
-    "function add64(uint256 a, uint256 b, address cOwner) external"
-]);
-console.log("Selector for add64:", iface.getFunction("add64")!.selector);
